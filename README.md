c5d09d933da3e1dc56aff107503e5075624c8e0f
Alessandro Visotto <visotto@users.noreply.github.com>
2014-04-12 00:12:09 -0300

    Adiciona README, verificar. Modifica indentação

---
+++ README.md
@@ -0,0 +1,35 @@
+Trabalho ED 1
+=============
+
+##Enunciado
+Implemente a soma, subtração e multiplicação de dois números grandes ( n > 10^100 ) com a possibilidade de entrada de números negativos. Utilize listas encadeadas. Você pode usar lista encadeada com ou sem nó cabeça, duplamente encadeada e/ou circular.
+
+O seu programa deve ler os dígitos separados por um espaço, de 2 números n1 e n2 e imprimir o resultado de n1 + n2, n1 - n2, n1 * n2, nessa ordem com um "\n" entre os resultados. Note que o número de dígitos de n1 e n2 não necessariamente é igual.
+
+Por exemplo, para a entrada (para simplificar, neste exemplo, o número não é grande como requerido):
+5
+5 4 3 2 1
+4
+2 3 4 5
+
+Saída:
+56666
+51976
+127382745
+
+Você deve desenvolver a biblioteca com as operações da lista escolhida. A implementação das operações da aplicação (leitura dos números, soma, multiplicação e subtração, apresentação do resultado) deve ser implementado em um outro arquivo .c.
+
+Você deve entregar o .c e .h da biblioteca de lista, .c com a implementação da aplicação e o Makefile. Formato do arquivo de entrega: trab1-Nome.zip (ou .tgz).
+
+
+
+##Versões
+
+######1.0.2
+Indentação refeita, chamadas de funções que estavam comentadas no main apagadas.
+
+Função soma(Lista *n1, Lista *n2) foi corrigida adicionando o caso especial em que ocorre overflow no bit mais significativo: 99+1 = 100, Lista *resultado possui 3 dígitos.
+
+
+##OBS
+Arquivo verificar possui casos de testes já realizados.
--- trabED.c
+++ trabED.c
@@ -8,289 +8,289 @@ armazenar inicio, final e qtd de elementos da lista
 #include <stdlib.h>
 
 typedef struct elemento {
-	struct elemento *ant;
-	int algarismo;
-	struct elemento *prox;
+  struct elemento *ant;
+  int algarismo;
+  struct elemento *prox;
 } Elem;
 
 typedef struct descritor {
-	struct elemento *inicio;
-	struct elemento *final;
-	int qtd;
+  struct elemento *inicio;
+  struct elemento *final;
+  int qtd;
 } Lista;
 
 Lista* cria_lista() {
-	Lista* li = (Lista*) malloc(sizeof(Lista));
-	if(li != NULL) {
-		li->inicio = NULL;
-		li->final = NULL;
-		li->qtd = 0;
-	}
-	return li;
+  Lista* li = (Lista*) malloc(sizeof(Lista));
+  if(li != NULL) {
+    li->inicio = NULL;
+    li->final = NULL;
+    li->qtd = 0;
+  }
+  return li;
 }
 
 int insere_inicio(Lista* li, int alg) {
-	if(li == NULL)
-		return 0;
-		
-	Elem *no = (Elem*) malloc(sizeof(Elem));
-	
-	if(no == NULL)
-		return 0;
-		
-	no->algarismo = alg;
-	no->ant = NULL;
-	no->prox = li->inicio;
-	li->inicio = no; // primeiro elemento da lista passa a ser o elemento no
-	if(li->qtd == 0) { // lista vazia: insere inicio
-		li->final = no;
-	} 
-	li->qtd=li->qtd+1;
-	return 1;
+  if(li == NULL)
+    return 0;
+    
+  Elem *no = (Elem*) malloc(sizeof(Elem));
+  
+  if(no == NULL)
+    return 0;
+    
+  no->algarismo = alg;
+  no->ant = NULL;
+  no->prox = li->inicio;
+  li->inicio = no; // primeiro elemento da lista passa a ser o elemento no
+  if(li->qtd == 0) { // lista vazia: insere inicio
+    li->final = no;
+  } 
+  li->qtd=li->qtd+1;
+  return 1;
 }
 
 int insere_fim(Lista* li, int alg) {
-	if(li == NULL)
-		return 0;
-		
-	Elem *no = (Elem*) malloc(sizeof(Elem));
-	
-	if(no == NULL)
-		return 0;
-		
-	no->algarismo = alg;
-	no->prox = NULL;
-	
-	if(li->qtd == 0) { // lista vazia: insere inicio
-		no->ant = NULL;
-		li->inicio = no; // primeiro elemento da lista passa a ser o elemento no
-	} else {
-		Elem *aux = li->inicio; // aux (ponteiro auxiliar) recebe inicio da lista
-		
-		while (aux->prox != NULL) // busca onde inserir
-			aux = aux->prox;
-		aux->prox = no; // insere depois de aux
-		no->ant = aux;
-	}
-	li->final = no;
-	li->qtd=li->qtd+1;
-	return 1;
+  if(li == NULL)
+    return 0;
+    
+  Elem *no = (Elem*) malloc(sizeof(Elem));
+  
+  if(no == NULL)
+    return 0;
+    
+  no->algarismo = alg;
+  no->prox = NULL;
+  
+  if(li->qtd == 0) { // lista vazia: insere inicio
+    no->ant = NULL;
+    li->inicio = no; // primeiro elemento da lista passa a ser o elemento no
+  } else {
+    Elem *aux = li->inicio; // aux (ponteiro auxiliar) recebe inicio da lista
+    
+    while (aux->prox != NULL) // busca onde inserir
+      aux = aux->prox;
+    aux->prox = no; // insere depois de aux
+    no->ant = aux;
+  }
+  li->final = no;
+  li->qtd=li->qtd+1;
+  return 1;
 }
 
 int lista_vazia(Lista *li) {
-	if(li->qtd == 0)
-		return 1;
-	return 0;
+  if(li->qtd == 0)
+    return 1;
+  return 0;
 }
 
 void imprime(Lista *li) {
-	Elem *aux = li->inicio;
-	int i;
-	if(lista_vazia(li)) {
-		printf("lista vazia\n");
-		return;
-	}
-	for(i = 0; i < li->qtd; i++) {
-		printf("%d ", aux->algarismo);
-		aux = aux->prox;
-	}
-	printf("\n");
+  Elem *aux = li->inicio;
+  int i;
+  if(lista_vazia(li)) {
+    printf("lista vazia\n");
+    return;
+  }
+  for(i = 0; i < li->qtd; i++) {
+    printf("%d ", aux->algarismo);
+    aux = aux->prox;
+  }
+  printf("\n");
 }
 
 void libera(Lista *li) {
-	Elem *aux;
-	int i;
-	if(lista_vazia(li)) {
-		return;
-	}
-	for(i = 0; i < li->qtd; i++) {
-		aux = li->inicio;
-		li->inicio = aux->prox;
-		free(aux);
-	}
-	li->qtd = 0;
+  Elem *aux;
+  int i;
+  if(lista_vazia(li)) {
+    return;
+  }
+  for(i = 0; i < li->qtd; i++) {
+    aux = li->inicio;
+    li->inicio = aux->prox;
+    free(aux);
+  }
+  li->qtd = 0;
 }
 
 // retorna o algarismo presente do no desejado
 int retorna_algarismo(Lista *li, int posicao) {
-	Elem *aux = li->inicio;
-	int i;
-	for(i = 0; i < posicao; i++) {
-		aux = aux->prox;
-	}
-//	printf("%d\n", aux->algarismo);
-	return aux->algarismo;
+  Elem *aux = li->inicio;
+  int i;
+  for(i = 0; i < posicao; i++) {
+    aux = aux->prox;
+  }
+//  printf("%d\n", aux->algarismo);
+  return aux->algarismo;
 }
 
 // soma listas que iniciam com o mesmo sinal
 Lista* soma(Lista *n1, Lista *n2) {
-	Lista* resultado = cria_lista();
-	int i;
-	int j; // carry
-	
-	if(n1->qtd < n2->qtd) {
-		while(n1->qtd != n2->qtd) {
-			insere_inicio(n1, 0);
-		}
-	} else {
-		while(n1->qtd != n2->qtd) {
-			insere_inicio(n2, 0);
-		}
-	}
+  Lista* resultado = cria_lista();
+  int i;
+  int j; // carry
+  
+  if(n1->qtd < n2->qtd) {
+    while(n1->qtd != n2->qtd) {
+      insere_inicio(n1, 0);
+    }
+  } else {
+    while(n1->qtd != n2->qtd) {
+      insere_inicio(n2, 0);
+    }
+  }
 
-	j = 0;
-	for(i = n1->qtd - 1; i >= 0; i--) { // -1 devido as posicoes serem de 0 a n-1
-		if(retorna_algarismo(n1, i) + retorna_algarismo(n2, i) + j <= 9) {
-			insere_inicio(resultado, retorna_algarismo(n1, i) + retorna_algarismo(n2, i) + j);
-			j = 0;
-		} else {
-			insere_inicio(resultado, retorna_algarismo(n1, i) + retorna_algarismo(n2, i) + j - 10);
-			j = 1;
-			if(i==0)
-				insere_inicio(resultado, 1); // se resultado tiver carry no algarismo mais significativo
-		}
-	}
-	return resultado;
+  j = 0;
+  for(i = n1->qtd - 1; i >= 0; i--) { // -1 devido as posicoes serem de 0 a n-1
+    if(retorna_algarismo(n1, i) + retorna_algarismo(n2, i) + j <= 9) {
+      insere_inicio(resultado, retorna_algarismo(n1, i) + retorna_algarismo(n2, i) + j);
+      j = 0;
+    } else {
+      insere_inicio(resultado, retorna_algarismo(n1, i) + retorna_algarismo(n2, i) + j - 10);
+      j = 1;
+      if(i==0)
+        insere_inicio(resultado, 1); // se resultado tiver carry no algarismo mais significativo
+    }
+  }
+  return resultado;
 }
 
 // subtrai duas listas
 Lista *subtracao(Lista *n1, Lista *n2) {
-	Lista *resultado = cria_lista();
-	int i;
-	int j; // carry
-	
-	if(n1->qtd < n2->qtd) {
-		while(n1->qtd != n2->qtd) {
-			insere_inicio(n1, 0);
-		}
-	} else {
-		while(n1->qtd != n2->qtd) {
-			insere_inicio(n2, 0);
-		}
-	}
-	
-	j = 0;
-	for(i = n1->qtd - 1; i >= 0; i--) { // -1 devido as posicoes serem de 0 a n-1
-		if(retorna_algarismo(n1, i) + j >= retorna_algarismo(n2, i)) {
-			insere_inicio(resultado, retorna_algarismo(n1, i) + j - retorna_algarismo(n2, i));
-			j = 0;
-		} else {
-			insere_inicio(resultado, retorna_algarismo(n1, i) + j + 10 - retorna_algarismo(n2, i));
-			j = -1;
-		}
-	}
-	
-	return resultado;
-	
+  Lista *resultado = cria_lista();
+  int i;
+  int j; // carry
+  
+  if(n1->qtd < n2->qtd) {
+    while(n1->qtd != n2->qtd) {
+      insere_inicio(n1, 0);
+    }
+  } else {
+    while(n1->qtd != n2->qtd) {
+      insere_inicio(n2, 0);
+    }
+  }
+  
+  j = 0;
+  for(i = n1->qtd - 1; i >= 0; i--) { // -1 devido as posicoes serem de 0 a n-1
+    if(retorna_algarismo(n1, i) + j >= retorna_algarismo(n2, i)) {
+      insere_inicio(resultado, retorna_algarismo(n1, i) + j - retorna_algarismo(n2, i));
+      j = 0;
+    } else {
+      insere_inicio(resultado, retorna_algarismo(n1, i) + j + 10 - retorna_algarismo(n2, i));
+      j = -1;
+    }
+  }
+  
+  return resultado;
+  
 }
 
 // compara as listas n1 e n2 e retorna a maior
 // retorna 1 se n1 maior ou retorna -1 se n2 maior ou retorna 0 se forem iguais
 int maior(Lista *n1, Lista *n2) {
-	int i;
-	if(n1->inicio->algarismo > 0 && n2->inicio->algarismo < 0)
-		return 1;
-	if(n2->inicio->algarismo > 0 && n1->inicio->algarismo < 0)
-			return -1;
-	
-	if(n1->inicio->algarismo > 0 && n2->inicio->algarismo > 0) {
-		if(n1->qtd > n2-> qtd) {
-			return 1;
-		}
-		if(n1->qtd < n2->qtd) {
-			return -1;
-		}
-		
-		for(i = 1; i < n1->qtd; i++) {
-			if(retorna_algarismo(n1, i) < retorna_algarismo(n2, i)) {
-				return -1;
-			}
-			if(retorna_algarismo(n1, i) > retorna_algarismo(n2, i)) {
-				return 1;
-			}
-		}
-		return 0;
-	} else {
-		if(n1->qtd > n2->qtd) {
-				return -1;
-			}
-			if(n1->qtd < n2->qtd) {
-				return 1;
-			}
-			
-			for(i = 1; i < n1->qtd; i++) {
-				if(retorna_algarismo(n1, i) < retorna_algarismo(n2, i)) {
-					return 1;
-				}
-				if(retorna_algarismo(n1, i) > retorna_algarismo(n2, i)) {
-					return -1;
-				}
-			}
-			return 0;
-	}
+  int i;
+  if(n1->inicio->algarismo > 0 && n2->inicio->algarismo < 0)
+    return 1;
+  if(n2->inicio->algarismo > 0 && n1->inicio->algarismo < 0)
+      return -1;
+  
+  if(n1->inicio->algarismo > 0 && n2->inicio->algarismo > 0) {
+    if(n1->qtd > n2-> qtd) {
+      return 1;
+    }
+    if(n1->qtd < n2->qtd) {
+      return -1;
+    }
+    
+    for(i = 1; i < n1->qtd; i++) {
+      if(retorna_algarismo(n1, i) < retorna_algarismo(n2, i)) {
+        return -1;
+      }
+      if(retorna_algarismo(n1, i) > retorna_algarismo(n2, i)) {
+        return 1;
+      }
+    }
+    return 0;
+  } else {
+    if(n1->qtd > n2->qtd) {
+        return -1;
+      }
+      if(n1->qtd < n2->qtd) {
+        return 1;
+      }
+      
+      for(i = 1; i < n1->qtd; i++) {
+        if(retorna_algarismo(n1, i) < retorna_algarismo(n2, i)) {
+          return 1;
+        }
+        if(retorna_algarismo(n1, i) > retorna_algarismo(n2, i)) {
+          return -1;
+        }
+      }
+      return 0;
+  }
 }
 
 void operacao(Lista *n1, Lista *n2) {
-	Lista *resultado;
-	int i;
-	
-	// soma
-	if(n1->inicio->algarismo > 0 && n2->inicio->algarismo > 0) { // n1 e n2 positivo
-		resultado = soma(n1, n2);
-//		printf("fn1");
-	} else if(n1->inicio->algarismo < 0 && n2->inicio->algarismo < 0) { // n1 e n2 negativo
-		n1->inicio->algarismo *= -1;
-		n2->inicio->algarismo *= -1;
-		resultado = soma(n1, n2);
-		resultado->inicio->algarismo *= -1;
-		n1->inicio->algarismo *= -1;
-		n2->inicio->algarismo *= -1;
-//		printf("fn2");
-	} else if(n1->inicio->algarismo > 0 && n2->inicio->algarismo < 0) { // n1 positivo e n2 negativo
-		n2->inicio->algarismo *= -1;
-//		printf("fn3");
-		if(maior(n1, n2) == -1) {
-			resultado = subtracao(n2, n1);
-			resultado->inicio->algarismo *= -1;
-		} else {
-			resultado = subtracao(n1, n2);
-		}
-	} else { // n1 negativo e n2 positivo
-//		printf("fn4");
-		n1->inicio->algarismo *= -1;
-		if(maior(n1, n2) == 1) {
-			resultado = subtracao(n1, n2);
-			resultado->inicio->algarismo *= -1;
-		} else {
-			resultado = subtracao(n2, n1);
-		}
-	}
-	imprime(resultado);
-	// fim soma
+  Lista *resultado;
+  int i;
+  
+  // soma
+  if(n1->inicio->algarismo > 0 && n2->inicio->algarismo > 0) { // n1 e n2 positivo
+    resultado = soma(n1, n2);
+//    printf("fn1");
+  } else if(n1->inicio->algarismo < 0 && n2->inicio->algarismo < 0) { // n1 e n2 negativo
+    n1->inicio->algarismo *= -1;
+    n2->inicio->algarismo *= -1;
+    resultado = soma(n1, n2);
+    resultado->inicio->algarismo *= -1;
+    n1->inicio->algarismo *= -1;
+    n2->inicio->algarismo *= -1;
+//    printf("fn2");
+  } else if(n1->inicio->algarismo > 0 && n2->inicio->algarismo < 0) { // n1 positivo e n2 negativo
+    n2->inicio->algarismo *= -1;
+//    printf("fn3");
+    if(maior(n1, n2) == -1) {
+      resultado = subtracao(n2, n1);
+      resultado->inicio->algarismo *= -1;
+    } else {
+      resultado = subtracao(n1, n2);
+    }
+  } else { // n1 negativo e n2 positivo
+//    printf("fn4");
+    n1->inicio->algarismo *= -1;
+    if(maior(n1, n2) == 1) {
+      resultado = subtracao(n1, n2);
+      resultado->inicio->algarismo *= -1;
+    } else {
+      resultado = subtracao(n2, n1);
+    }
+  }
+  imprime(resultado);
+  // fim soma
 }
 
 int main() {
-	Lista* n1;
-	Lista* n2;
-	
-	n1 = cria_lista();
-	n2 = cria_lista();
+  Lista* n1;
+  Lista* n2;
+  
+  n1 = cria_lista();
+  n2 = cria_lista();
 
-	insere_fim(n1, 9);
-	insere_fim(n1, 1);
-	insere_fim(n1, 5);
-	insere_fim(n2, 9);
-	insere_fim(n2, 6);
-	
-	imprime(n1);
-	imprime(n2);
-	
-	operacao(n1, n2);
-	n2->inicio->algarismo *= -1;
-	operacao(n2, n1);
-	
-	libera(n1);
-	libera(n2);
-	
-	return 0;
+  insere_fim(n1, 9);
+  insere_fim(n1, 1);
+  insere_fim(n1, 5);
+  insere_fim(n2, 9);
+  insere_fim(n2, 6);
+  
+  imprime(n1);
+  imprime(n2);
+  
+  operacao(n1, n2);
+  n2->inicio->algarismo *= -1;
+  operacao(n2, n1);
+  
+  libera(n1);
+  libera(n2);
+  
+  return 0;
 }
